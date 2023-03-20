5ed808f53375ba044d2dec76f79f368d1b63af13
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-20 16:52:12 -0300

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,7 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-    this.Visiblity = 255;
+    this.Visiblity = 255; //totalmente visivel quando é chamado. Seu valor padrão
   }
 
  display(){
@@ -12,15 +12,11 @@ class Pig extends BaseClass {
    }
    else{
      World.remove(world, this.body);
-     push();
+     push(); //adiciona itens/objetos/sprites na matriz. Sempre vai se tornando o ultimo da fila
      this.Visiblity = this.Visiblity - 5;
-     tint(255,this.Visiblity);
+     tint(255,this.Visiblity); // 0 = invisivel, 255 = visivel
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-     pop();
-   }
-   
+     pop(); //retira itens/objetos/sprites na matriz. Sempre vai do ultimo para o penultimo para o antepenultimo.....
+   } 
  }
-
-
-
 };
