06203987ff63d1cc8028f7e2fc0ce244b195dbc9
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-20 16:59:21 -0300

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -13,8 +13,8 @@ class SlingShot{
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-    attach(body){
-        this.sling.bodyA = body;
+    attach(body){ //conectar o corpo
+        this.sling.bodyA = body; //conecta a corda ao corpo do pássaro
     }
     
     fly(){
